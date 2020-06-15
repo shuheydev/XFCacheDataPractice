14da9ef12cdd5e401d21656d83ff3a2ef9f317bf
Shuhey <shuhey.dev@gmail.com>
2020-06-16 00:24:19 +0900

    Add project files.

---
+++ XFCacheDataPractice/XFCacheDataPractice/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XFCacheDataPractice.Models.Covid19;
+using XFCacheDataPractice.Services;
+
+namespace XFCacheDataPractice
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private bool _isRefreshing;
+        public bool IsRefreshing
+        {
+            get => _isRefreshing;
+            set
+            {
+                _isRefreshing = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Prefecture> _prefectures;
+        public ObservableCollection<Prefecture> Prefectures
+        {
+            get => _prefectures;
+            set
+            {
+                _prefectures = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private readonly Covid19JapanApiManager _dataService;
+
+        public ICommand RefreshDataCommand { get; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this._dataService = DependencyService.Get<Covid19JapanApiManager>();
+
+            this.RefreshDataCommand = new Command(async _ => await ExecuteRefresh());
+
+            this.BindingContext = this;
+        }
+
+        private async Task ExecuteRefresh()
+        {
+            this.IsRefreshing = true;
+            await RefreshData();
+            //Refreshing中のぐるぐるが小さくなって消えるアニメーションをしっかり表示させる場合は
+            //Delayを挟むと良い.なぜかはわからない.
+            //1秒は個人的に自然な長さに見えたので
+            await Task.Delay(1000);
+            this.IsRefreshing = false;
+        }
+
+
+        private async Task RefreshData()
+        {
+            var prefecturesData = await this._dataService.GetPrefectures();
+
+            if (prefecturesData != null)
+                this.Prefectures = new ObservableCollection<Prefecture>(prefecturesData);
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await ExecuteRefresh();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await ExecuteRefresh();
+        }
+    }
+}
---
+++ XFCacheDataPractice/XFCacheDataPractice/Services/DataService.cs
@@ -0,0 +1,73 @@
+﻿using MonkeyCache;
+using MonkeyCache.FileStore;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using XFCacheDataPractice.Models.Covid19;
+using XFCacheDataPractice.Services;
+
+[assembly: Dependency(typeof(Covid19JapanApiManager))]
+namespace XFCacheDataPractice.Services
+{
+    public class Covid19JapanApiManager
+    {
+        private IBarrel _barrel;
+
+        private readonly HttpClient _httpClient;
+
+        public Covid19JapanApiManager()
+        {
+            this._httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://covid19-japan-web-api.now.sh/api/v1/")
+            };
+
+            Barrel.ApplicationId = AppInfo.PackageName;
+            this._barrel = Barrel.Create(FileSystem.AppDataDirectory);
+        }
+
+        public const string CachePrefecturesKey = "get_prefectures";
+
+        public async Task<IEnumerable<Prefecture>> GetPrefectures()
+        {
+            try
+            {
+                //キャッシュが存在して,有効期間内の場合はキャッシュから
+                if (!_barrel.IsExpired(key: CachePrefecturesKey))
+                {
+                    await Task.Yield();
+                    return _barrel.Get<IEnumerable<Prefecture>>(key: CachePrefecturesKey);
+                }
+
+                //インターネットに接続してれば取得
+                if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+                {
+                    var prefecturesData = await _httpClient.GetFromJsonAsync<IEnumerable<Prefecture>>("prefectures");
+
+                    //キャッシュを更新
+                    _barrel.Add(key: CachePrefecturesKey, data: prefecturesData, expireIn: TimeSpan.FromMinutes(10));
+
+                    return prefecturesData;
+                }
+
+                //インターネットにも接続してないし,キャッシュも有効期限切れだった場合
+                //それでもキャッシュデータがあれば返すか?
+                if (_barrel.Exists(key: CachePrefecturesKey))
+                {
+                    return _barrel.Get<IEnumerable<Prefecture>>(key: CachePrefecturesKey);
+                }
+
+                return new List<Prefecture>();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
